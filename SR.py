582fee1551b9d7f19a2d220f3f8fe76d0ae5251b
Pierre Paga <pierre.paga@gmail.com>
2018-02-06 15:02:08 +0000

    added readme to local

--- SR.py
+++ SR.py
@@ -17,87 +17,69 @@ flags = tf.app.flags
 flags.DEFINE_integer("LR_size", 32, "The size of low resolution patch to use [32]")
 flags.DEFINE_integer("HR_size", 128, "The size of the high resolution patch to produce [128]")
 flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_string("data_dir", "../../datasets/birds", "Directory name to save the image samples [../../datasets/birds]")
+flags.DEFINE_string("checkpoint_dir", None, "checkpoints directory path [None]")
+flags.DEFINE_string("sample_path", "./upscaled_image", "path to save the upscaled image [./upscaled_image]")
+flags.DEFINE_string("image_path", "./image", "path to the image to be upscaled [./image]")
 FLAGS = flags.FLAGS
 
-def image_choice(n_images, path):
-    LR_dim = 32
-    paths = glob(path + '/*.jpg')
-    images_paths = random.sample(paths, n_images)
+def image_SR(image, batch_size, sess, generator, NU, LR_images):
+#    generates both the image reconstructed by the network and the one standard upscaling would have returned.
     
-    images = []
-    shapes = np.zeros([n_images, 3])
-    for i in range(n_images):
-        image = imread(images_paths[i])
-        image = 2*(image.astype(float)/255.) - 1
-        images.append(image)
-        shapes[i,:] = image.shape
-    return images, shapes
-    
-def image_SR(images, batch_size, sess, generator, NU, LR_images):
+#    I could divide the image in patches of size 32x32, but there is a chance doing so would create visible lines between each patch. So instead, I use overlapping patches.
+#    The parameter k controls the amount of overlap.
+    k=2
     LR_dim = 32
-    upscaled_images = []
-    naive_upscaled = []
-    index = 0
-    for image in images:
-        print('processing image '+str(index)+' of '+str(len(images)))
-        index+=1
-        image_shape  = image.shape
-        Nx,Ny = image_shape[0] // LR_dim, image_shape[1] // LR_dim
-        LR_patch = np.zeros((1, LR_dim, LR_dim, 3))
-        upscaled_image = np.zeros((Nx*LR_dim*4, Ny*LR_dim*4,3))
-        naive_upscaling = np.zeros((Nx*LR_dim*4, Ny*LR_dim*4,3))
-        for y in range(2*Ny-1):
-            for x in range(2*Nx-1):
-                LR_patch[0,:,:,:] = image[x*LR_dim//2:(x+2)*LR_dim//2,y*LR_dim//2:(y+2)*LR_dim//2,:]
-                img, nimg = sess.run([generator.outputs, NU.outputs], feed_dict={LR_images : LR_patch})
-                upscaled_image[x*LR_dim*2:(x+2)*LR_dim*2,y*LR_dim*2:(y+2)*LR_dim*2,:] += img[0,:,:,:].squeeze()
-                naive_upscaling[x*LR_dim*2:(x+2)*LR_dim*2,y*LR_dim*2:(y+2)*LR_dim*2,:] += nimg[0,:,:,:].squeeze()
-        upscaled_image[1*LR_dim*2:(2*Nx-1)*LR_dim*2,:,:]/=2
-        upscaled_image[:,1*LR_dim*2:(2*Ny-1)*LR_dim*2,:]/=2
-        upscaled_images.append(upscaled_image)
-        naive_upscaling[1*LR_dim*2:(2*Nx-1)*LR_dim*2,:,:]/=2
-        naive_upscaling[:,1*LR_dim*2:(2*Ny-1)*LR_dim*2,:]/=2
-        naive_upscaled.append(naive_upscaling)
+    image_shape  = image.shape
+    
+    print('processing image')
+    Nx,Ny = image_shape[0] // LR_dim, image_shape[1] // LR_dim
+    LR_patch = np.zeros((1, LR_dim, LR_dim, 3))
+    upscaled_image = np.zeros((Nx*LR_dim*4, Ny*LR_dim*4,3))
+    naive_upscaling = np.zeros((Nx*LR_dim*4, Ny*LR_dim*4,3))
+    scale = np.zeros((Nx*LR_dim*4, Ny*LR_dim*4,3))
+    for y in range(k*Ny-1):
+        for x in range(k*Nx-1):
+            LR_patch[0,:,:,:] = image[x*LR_dim//k:(x+k)*LR_dim//k,y*LR_dim//k:(y+k)*LR_dim//k,:]
+            img, nimg = sess.run([generator.outputs, NU.outputs], feed_dict={LR_images : LR_patch})
+            upscaled_image[(x*LR_dim*4)//k:((x+k)*LR_dim*4)//k,(y*LR_dim*4)//k:((y+k)*LR_dim*4)//k,:] += img[0,:,:,:].squeeze()
+            naive_upscaling[(x*LR_dim*4)//k:((x+k)*LR_dim*4)//k,(y*LR_dim*4)//k:((y+k)*LR_dim*4)//k,::] += nimg[0,:,:,:].squeeze()
+            scale[(x*LR_dim*4)//k:((x+k)*LR_dim*4)//k,(y*LR_dim*4)//k:((y+k)*LR_dim*4)//k,:] += 1
+    upscaled_image = upscaled_image/scale
+    naive_upscaling = naive_upscaling/scale
     print('processing finished')
-    return upscaled_images, naive_upscaled
+    return upscaled_image, naive_upscaling
 
 def main(_):
     
     pp.pprint(flags.FLAGS.__flags)
     
     path = FLAGS.data_dir
-    image_list, shapes = image_choice(6, path)
     batch_size = int(np.max(shapes[:,1]))//FLAGS.LR_size
     
     with tf.device("/gpu:0"):
         LR_images =  tf.placeholder(tf.float32, [None, FLAGS.LR_size, FLAGS.LR_size, FLAGS.c_dim], name='LR_images')
         generator, naive_upscaling = model.generator(LR_images, is_train=False, reuse=False)
    
-    sess = tf.InteractiveSession()
-    tl.layers.initialize_global_variables(sess)
+    g_vars = tl.layers.get_variables_with_name('generator', True, False)
+    saver = tf.train.Saver(g_vars)
     
-    model_dir = "%s_%s" % (32, FLAGS.LR_size)
-    save_dir = os.path.join(FLAGS.checkpoint_dir, model_dir)
+    image_path = FLAGS.image_path
     
-    # load the latest checkpoints
-    net_g_name = os.path.join(save_dir, 'net_g.npz')
-    net_d_name = os.path.join(save_dir, 'net_d.npz')
-
-    if tl.files.file_exists(net_g_name):
-        load_params_generator = tl.files.load_npz(name=net_g_name)
-        tl.files.assign_params(sess, load_params_generator, generator)
+    image = imread(images_path)
+    image = 2*(image.astype(float)/255.) - 1
+    
+    with tf.Session() as sess: 
+        tl.layers.initialize_global_variables(sess)    
+        # load the latest checkpoints
+        if tl.files.file_exists(checkpoint_dir+'checkpoint'):
+            saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
         print('generator model loaded')
-        
-    path = FLAGS.data_dir
-    upscaled_images, naive_upscaled = image_SR(image_list, batch_size, sess, generator, naive_upscaling, LR_images)
-    original_images = image_list
-    for i in range(6):
-        tl.visualize.save_image(upscaled_images[i], './images/upscaled'+str(i)+'.png')
-        tl.visualize.save_image(naive_upscaled[i], './images/naive'+str(i)+'.png')
-        tl.visualize.save_image(image_list[i], './images/original'+str(i)+'.png')
+            
+        path = FLAGS.data_dir
+        upscaled_image, naive_upscaled = image_SR(image_list, batch_size, sess, generator, naive_upscaling, LR_images)
+            tl.visualize.save_image(upscaled_images, './images/upscaled'+str(i)+'.png')
+            tl.visualize.save_image(naive_upscaled, './images/naive'+str(i)+'.png')
+            tl.visualize.save_image(image_list, './images/original'+str(i)+'.png')
 
 if __name__ == '__main__':
     tf.app.run()
--- main.py
+++ main.py
@@ -11,14 +11,13 @@ from utils import *
 
 from keras.applications import vgg19
 
-
-
 pp = pprint.PrettyPrinter()
 
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
 flags.DEFINE_integer("updates", 100, "Updates per epoch [100]")
 flags.DEFINE_float("learning_rate", 0.001, "initial Learning rate for adam [0.1]")
+flags.DEFINE_float("decay_rate", .9, "learning rate decay per thousand updates - set to 1. for constant learning rate [0.9]")
 flags.DEFINE_float("beta1", 0.9, "Momentum term of adam [0.9]")
 flags.DEFINE_integer("batch_size", 16, "The number of batch images [16]")
 flags.DEFINE_integer("LR_size", 32, "The size of low resolution patch to use [32]")
@@ -63,9 +62,6 @@ class vgg_loss():
         loss = loss/len(features)
         return loss
 
-        
-    
-
 def main(_):
     pp.pprint(flags.FLAGS.__flags)
     
@@ -73,64 +69,57 @@ def main(_):
     g_perception_scaling = FLAGS.perception_scaling
     d_lr_scale = FLAGS.discriminator_lr_scale    
 
-    with tf.device('/GPU:0'):
-        ##========================= DEFINE MODEL ===========================##
-        HR_images = tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.HR_size, FLAGS.HR_size, FLAGS.c_dim], name='HR_images')
-        LR_images =  tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.LR_size, FLAGS.LR_size, FLAGS.c_dim], name='LR_images')
-        
-    #   HR_images =  tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.HR_size, FLAGS.HR_size, FLAGS.c_dim], name='HR_images')
-
-        # gen for training
-        generator, _ = model.generator(LR_images, is_train=True, reuse=False)
-        # generated fake images --> discriminator
-        discriminator_fake = model.discriminator(generator.outputs, is_train=True, reuse=False)
-        # real images --> discriminator
-        discriminator_real = model.discriminator(HR_images, is_train=True, reuse=True)
-        # sample_z --> gen for evaluation, set is_train to False so that BatchNormLayer behave differently
-        sample_gen, naive_upscaler = model.generator(LR_images, is_train=False, reuse=True)
-        upscaled_image = naive_upscaler.outputs
-
-        ##========================= DEFINE TRAIN OPS =======================##
-        # cost for updating discriminator and gen
-        
-        dr_logits= discriminator_real.outputs
-        df_logits= discriminator_fake.outputs
-        
-        # discriminator: real images are labelled as 1
-        d_loss_real = tl.cost.sigmoid_cross_entropy(dr_logits, tf.ones_like(dr_logits), name='dreal')
-        # discriminator: images from gen (fake) are labelled as 0
-        d_loss_fake = tl.cost.sigmoid_cross_entropy(df_logits, tf.zeros_like(df_logits), name='dfake')
-        d_loss = d_loss_real + d_loss_fake
-        
-        # gen: try to make the the fake images look real (1)
-        g_loss_adversarial = tl.cost.sigmoid_cross_entropy(df_logits, tf.ones_like(df_logits), name='gfake')
-    #    g_loss_content = tl.cost.mean_squared_error(generator.outputs, HR_images, is_mean = True)
-        
-    #    build the VGG perception loss
-        vgg = vgg_loss([128,128,3])
-        with tf.variable_scope("perception", reuse = False):
-            tl.layers.set_name_reuse(False)
-            perception_input = InputLayer(HR_images, name='perception_input')
-            perception_output = LambdaLayer(perception_input, fn=vgg.features, name='perception_output')       
-        with tf.variable_scope("perception", reuse = True):
-            tl.layers.set_name_reuse(True)
-            perception_input_gen = InputLayer(generator.outputs, name='perception_input_gen')
-            perception_output_gen = LambdaLayer(perception_input_gen, fn=vgg.features, name='perception_output_gen')       
-        features_real = perception_output.outputs
-        features_gen = perception_output_gen.outputs
-        g_loss_perception = vgg.loss(features_real, features_gen)
-
-#    scales the perception and content losses so that their gradients are comparable (and of order 1)
-#    g_loss_perception = g_loss_perception
-#    g_loss_content = g_content_scaling*g_loss_content
-#    total generator loss
-    g_loss = g_loss_adversarial/g_perception_scaling + g_loss_perception
-#    g_loss = g_loss + g_loss_content
+#    with tf.device('/GPU:0'):
+    ##========================= DEFINE MODEL ===========================##
+    HR_images = tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.HR_size, FLAGS.HR_size, FLAGS.c_dim], name='HR_images')
+    LR_images =  tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.LR_size, FLAGS.LR_size, FLAGS.c_dim], name='LR_images')
     
-#    generator.print_params(False)
-#    print("---------------")
-#    discriminator_fake.print_params(False)
+#   HR_images =  tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.HR_size, FLAGS.HR_size, FLAGS.c_dim], name='HR_images')
 
+    # gen for training
+    generator, _ = model.generator(LR_images, is_train=True, reuse=False)
+    # generated fake images --> discriminator
+    discriminator_fake = model.discriminator(generator.outputs, is_train=True, reuse=False)
+    # real images --> discriminator
+    discriminator_real = model.discriminator(HR_images, is_train=True, reuse=True)
+    # sample_z --> gen for evaluation, set is_train to False so that BatchNormLayer behave differently
+    sample_gen, naive_upscaler = model.generator(LR_images, is_train=False, reuse=True)
+    # the result of a basic upsampling is generated for reference
+    upscaled_image = naive_upscaler.outputs
+
+    ##========================= DEFINE TRAIN OPS =======================##
+    
+    dr_logits= discriminator_real.outputs
+    df_logits= discriminator_fake.outputs
+    
+    # discriminator: real images are labelled as 1
+    d_loss_real = tl.cost.sigmoid_cross_entropy(dr_logits, tf.ones_like(dr_logits), name='dreal')
+    # discriminator: images from the generator (fake) are labelled as 0
+    d_loss_fake = tl.cost.sigmoid_cross_entropy(df_logits, tf.zeros_like(df_logits), name='dfake')
+    # total loss is the sum of the previous two
+    d_loss = d_loss_real + d_loss_fake
+    
+    # adversarial generator loss: try to make the the fake images look real (1)
+    g_loss_adversarial = tl.cost.sigmoid_cross_entropy(df_logits, tf.ones_like(df_logits), name='gfake')
+    
+#    build the VGG perception loss
+    vgg = vgg_loss([128,128,3])
+    with tf.variable_scope("perception", reuse = False):
+        tl.layers.set_name_reuse(False)
+        perception_input = InputLayer(HR_images, name='perception_input')
+        perception_output = LambdaLayer(perception_input, fn=vgg.features, name='perception_output')       
+    with tf.variable_scope("perception", reuse = True):
+        tl.layers.set_name_reuse(True)
+        perception_input_gen = InputLayer(generator.outputs, name='perception_input_gen')
+        perception_output_gen = LambdaLayer(perception_input_gen, fn=vgg.features, name='perception_output_gen')       
+    features_real = perception_output.outputs
+    features_gen = perception_output_gen.outputs
+    g_loss_perception = vgg.loss(features_real, features_gen)
+
+    g_loss = g_loss_adversarial/g_perception_scaling + g_loss_perception
+    
+    
+    # list of generator and discriminator variables for use by the optimizers
     g_vars = tl.layers.get_variables_with_name('generator', True, False)
     d_vars = tl.layers.get_variables_with_name('discriminator', True, False)
     
@@ -139,41 +128,25 @@ def main(_):
 
     global_step = tf.Variable(0, trainable=False)
     starter_learning_rate = FLAGS.learning_rate
-    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,decay_steps = 2*1e3, decay_rate = .5, staircase=False)
+    decay_rate = FLAGS.decay_rate
+    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,decay_steps = 2*1e3, decay_rate = decay_rate, staircase=False)
 
-#    d_optim = tf.train.GradientDescentOptimizer(d_lr_scale*FLAGS.learning_rate).minimize(d_loss, var_list = d_vars)
     d_optim = tf.train.AdamOptimizer(d_lr_scale*learning_rate, beta1=FLAGS.beta1).minimize(d_loss, var_list = d_vars, global_step = global_step)
-
-#    d_grads = tf.gradients(d_loss, d_vars)
-#    d_grads_list = list(zip(d_grads, d_vars))
-
-#    g_optim = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(g_loss, var_list = g_vars)
     g_optim = tf.train.AdamOptimizer(learning_rate, beta1=FLAGS.beta1).minimize(g_loss, var_list = g_vars,global_step = global_step)
     
     g_grads_adversarial = tf.gradients(g_loss_adversarial, g_vars)
     g_grads_perception = tf.gradients(g_loss_perception, g_vars)
-#    g_grads_content = tf.gradients(g_loss_content, g_vars)
-#    g_grads = []
+
     with tf.name_scope('gradient_histograms/'):
-#        for g0,g1,g2 in zip(g_grads_adversarial, g_grads_content, g_grads_perception):
         for g0,g1, var in zip(g_grads_adversarial, g_grads_perception, g_vars):
-#            print(var)
             if not 'prelu' in var.name:
                 if not (('Reshape' in g0.op.name) or ('batchnorm' in g0.op.name)):
                     tf.summary.histogram('adversarial/'+g0.op.name, g0)
                     tf.summary.histogram('content/'+g1.op.name, g1)
-#                tf.summary.histogram('perception/'+g2.op.name, g2)
-    #        g_grads.append(g0+g1+g2)
-#            g_grads.append(g0+g1)
-#    g_grads_list = list(zip(g_grads, g_vars))
-
-
     
     with tf.name_scope('loss_summaries/'):
-#        tf.summary.scalar('generator_content', g_loss_content/g_content_scaling)
         tf.summary.scalar('generator_adversarial', g_loss_adversarial)
         tf.summary.scalar('generator_perception', g_loss_perception)
-#        tf.summary.scalar('generator_total', g_loss)
         tf.summary.scalar('discriminator_real', d_loss_real)
         tf.summary.scalar('discriminator_fake', d_loss_fake)
         tf.summary.scalar('discriminator_total', d_loss)
@@ -182,88 +155,90 @@ def main(_):
     merged = tf.summary.merge_all()    
     saver = tf.train.Saver(g_vars+d_vars)
         
-    sess = tf.InteractiveSession()
-    tl.layers.initialize_global_variables(sess)
+    with tf.Session() as sess:
     
-    run_dir = './runs/'+FLAGS.run_name+'/'    
-    checkpoint_dir = run_dir+'checkpoints/'
-    sample_dir = run_dir+'samples/'
-    logs_dir = run_dir+'logs/'
+        tl.layers.initialize_global_variables(sess)    
 
-    tl.files.exists_or_mkdir(run_dir)    
-    tl.files.exists_or_mkdir(checkpoint_dir)
-    tl.files.exists_or_mkdir(sample_dir)
-    tl.files.exists_or_mkdir(logs_dir)
-    
-    save_path = checkpoint_dir+'/model.ckpt'
-    print('the model will be saved at '+ save_path)
+        # generates the various folders in case they don't already exist, starting with the run directory
+
+        run_dir = './runs/'+FLAGS.run_name+'/'    
+        checkpoint_dir = run_dir+'checkpoints/'
+        sample_dir = run_dir+'samples/'
+        logs_dir = run_dir+'logs/'
+
+        tl.files.exists_or_mkdir(run_dir)    
+        tl.files.exists_or_mkdir(checkpoint_dir)
+        tl.files.exists_or_mkdir(sample_dir)
+        tl.files.exists_or_mkdir(logs_dir)
         
-    writer = tf.summary.FileWriter(logs_dir, graph=tf.get_default_graph())
+        #creates the tensorboard log writer
+        writer = tf.summary.FileWriter(logs_dir, graph=tf.get_default_graph())
 
-    # load the latest checkpoints
-    if FLAGS.load_from_checkpoint:
-        if tl.files.file_exists(checkpoint_dir+'checkpoint'):
-            print('loading model from '+checkpoint_dir)
-            saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
-            print('model loaded')
+        save_path = checkpoint_dir+'/model.ckpt'
+        print('the model will be saved at '+ save_path)
+
+        # load the latest checkpoints if load_from_checkpoint is set to True. Otherwise, generates a summary of the new network
+        if FLAGS.load_from_checkpoint:
+            if tl.files.file_exists(checkpoint_dir+'checkpoint'):
+                print('loading model from '+checkpoint_dir)
+                saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
+                print('model loaded')
+            else:
+                print('model not found, starting from scratch')
+                generator.print_params()
+                discriminator_fake.print_params()
         else:
-            print('model not found, starting from scratch')
             generator.print_params()
             discriminator_fake.print_params()
-    else:
-        generator.print_params()
-        discriminator_fake.print_params()
 
-    
-    path = FLAGS.data_dir   
-    shape = [FLAGS.HR_size, FLAGS.HR_size]
-    data_preproc = data_preprocessing(path,shape)
+        path = FLAGS.data_dir   
+        shape = [FLAGS.HR_size, FLAGS.HR_size]
+        
+        # creates the image pipeline
+        data_preproc = data_preprocessing(path,shape)
 
-    batchsize = FLAGS.batch_size
+#        sets the parameters for saving the sample images
+        batchsize = FLAGS.batch_size
+        n_display_0 = int(np.sqrt(batchsize))+1
+        n_display_1 = batchsize//n_display_0+1
 
-    n_display_0 = int(np.sqrt(batchsize))+1
-    n_display_1 = batchsize//n_display_0+1
-    
+        ##========================= TRAIN MODELS ================================##
+        naming_offset = FLAGS.naming_offset
+        iter_counter = naming_offset*FLAGS.updates
+        
+#        saves the reference images
+        HR_sample, LR_sample = data_preproc.batch(batchsize = batchsize, single_image=False)
+        up_img = sess.run(upscaled_image, feed_dict={LR_images : LR_sample, HR_images: HR_sample})
+        tl.visualize.save_images(up_img, [n_display_0, n_display_1+1], '{}train_LR.png'.format(sample_dir))
+        tl.visualize.save_images(HR_sample, [n_display_0, n_display_1+1], '{}train_HR.png'.format(sample_dir))
+        
+        for epoch in range(FLAGS.epoch):
+            epoch_time = time.time()
+            for update in range(FLAGS.updates):
+                start_time = time.time()
+                
+    #            generate low and high-resolution image batch:
+                HR_batch, LR_batch = data_preproc.batch(batchsize = batchsize, single_image=False)
 
-    ##========================= TRAIN MODELS ================================##
-    naming_offset = FLAGS.naming_offset
-    iter_counter = naming_offset*FLAGS.updates
-    
-    HR_sample, LR_sample = data_preproc.batch(batchsize = batchsize, single_image=False)
-    up_img = sess.run(upscaled_image, feed_dict={LR_images : LR_sample, HR_images: HR_sample})
-    tl.visualize.save_images(up_img, [n_display_0, n_display_1+1], '{}/train_LR.png'.format(sample_dir))
-    tl.visualize.save_images(HR_sample, [n_display_0, n_display_1+1], '{}/train_HR.png'.format(sample_dir))
-    for epoch in range(FLAGS.epoch):
-        epoch_time = time.time()
-        for update in range(FLAGS.updates):
-            start_time = time.time()
-            
-#            generate low and high-resolution image batch:
-            HR_batch, LR_batch = data_preproc.batch(batchsize = batchsize, single_image=False)
+    #            train the network and generate summary statistics for tensorboard
+                summary,_,_ = sess.run([merged,g_optim, d_optim], feed_dict={LR_images: LR_batch, HR_images: HR_batch })
+                print("Epoch: [%2d/%2d] [%3d/%3d]" % (epoch+naming_offset, FLAGS.epoch+naming_offset, update, FLAGS.updates))
+                writer.add_summary(summary, iter_counter)
+                
+                if epoch>0: # only save things if the training has actually started
+                    if np.mod(iter_counter, FLAGS.sample_step) == 0:
+                        # generate and visualize sample images
+                        img, _ = sess.run([sample_gen.outputs, upscaled_image], feed_dict={LR_images : LR_sample, HR_images: HR_sample})
+                        tl.visualize.save_images(img, [n_display_0, n_display_1+1], '{}train_{:02d}.png'.format(sample_dir, epoch+naming_offset))
+                        
+                    if np.mod(iter_counter, FLAGS.save_step) == 0:
+                        # save current network parameters
+                        saver.save(sess, save_path, global_step = (epoch+naming_offset)*FLAGS.updates+update)
+                        print("checkpoint saved at "+save_path)
+                iter_counter+=1
+            epoch_duration = time.time() - epoch_time
+            print("Epoch duration: %4.4f" % (epoch_duration))
+            print("estimated time to completion: %4.4f" % ((FLAGS.epoch - epoch)*(epoch_duration)/60), 'mins')
 
-#            train the network and generate summary statistics for tensorboard
-            summary,_,_ = sess.run([merged,g_optim, d_optim], feed_dict={LR_images: LR_batch, HR_images: HR_batch })
-#            _ = sess.run([d_grads_list, train_op_d], feed_dict={LR_images: LR_batch, HR_images: HR_batch })
-            print("Epoch: [%2d/%2d] [%3d/%3d]" % (epoch+naming_offset, FLAGS.epoch+naming_offset, update, FLAGS.updates))
-            writer.add_summary(summary, iter_counter)
-            if epoch>0: # only save things if the training has actually started
-                if np.mod(iter_counter, FLAGS.sample_step) == 0:
-                    # generate and visualize sample images, along with the HR version and the bilinearly upscaled version, and the difference between generated and upscaled images
-                    img, up_img = sess.run([sample_gen.outputs, upscaled_image], feed_dict={LR_images : LR_sample, HR_images: HR_sample})
-                    tl.visualize.save_images(img, [n_display_0, n_display_1+1], '{}train_{:02d}.png'.format(sample_dir, epoch+naming_offset))
-#                    tl.visualize.save_images(img - up_img, [n_display_0, n_display_1+1], '{}/train_diff_{:02d}.png'.format(sample_dir, epoch+naming_offset))
-                    
-                if np.mod(iter_counter, FLAGS.save_step) == 0:
-                    # save current network parameters
-                    saver.save(sess, save_path, global_step = (epoch+naming_offset)*FLAGS.updates+update)
-                    print("checkpoint saved at "+save_path)
-            iter_counter+=1
-        epoch_duration = time.time() - epoch_time
-        print("Epoch duration: %4.4f" % (epoch_duration))
-        print("estimated time to completion: %4.4f" % ((FLAGS.epoch - epoch)*(epoch_duration)/60), 'mins')
-#    if np.mod(iter_counter, FLAGS.save_step) == 0:
-        # save current network parameters
-    saver.save(sess, save_path, global_step = (epoch+naming_offset)*FLAGS.updates+update)
-    print("checkpoint saved at "+save_path)
 if __name__ == '__main__':
     tf.app.run()
